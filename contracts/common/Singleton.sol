f726d84148f625391ccb4c096a33189d88952c92
Charlie-Zhou <charliez@saharalabs.ai>
2024-09-24 14:38:28 +0800

    modified:   contracts/common/Singleton.sol

--- contracts/common/Singleton.sol
+++ contracts/common/Singleton.sol
@@ -1,13 +1,13 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: LGPL-3.0-only  
+pragma solidity >=0.7.0 <0.9.0;  
 
-/**
- * @title Singleton - Base for singleton contracts (should always be the first super contract)
- *        This contract is tightly coupled to our proxy contract (see `proxies/SafeProxy.sol`)
- * @author Richard Meissner - @rmeissner
- */
-abstract contract Singleton {
-    // singleton always has to be the first declared variable to ensure the same location as in the Proxy contract.
-    // It should also always be ensured the address is stored alone (uses a full word)
-    address private singleton;
+/**  
+ * @title Singleton - 单例合约的基础（应始终是第一个超类合约）  
+ *        该合约与我们的代理合约紧密耦合（参见 `proxies/SafeProxy.sol`）  
+ * @author Richard Meissner - @rmeissner  
+ */  
+abstract contract Singleton {  
+    // 单例必须始终是第一个声明的变量，以确保与代理合约中的位置相同。  
+    // 还应确保地址单独存储（使用一个完整字）。  
+    address private singleton;  
 }
