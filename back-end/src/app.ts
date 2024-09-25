e83c9f0771a405721176a6c688ea9147c0c8b048
Charlie-Zhou <charliez@saharalabs.ai>
2024-09-25 13:26:22 +0800

    chore(backend):update app.ts

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,10 +1,13 @@
 import express,{ Request,Response } from 'express';
 import { swaggerUi,specs } from './swagger';  
 import { sigRouter } from './routes/sigRouter';
+import { calldataRouter } from './routes/calldataRouter';
 const app = express();
 const port = 3000;
 
 app.use('/api',sigRouter);
+app.use('/api',calldataRouter);
+
 app.use(express.json());
 
 app.use('/swagger',swaggerUi.serve,swaggerUi.setup(specs));  
